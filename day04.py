b8cf1e8693a14f92c5ba0cbfdfe9625ecd28b05f
Adilbek <adilbekov0230@gmail.com>
2026-06-25 23:25:12 +0500

    Add for/while

---
+++ day04.py
@@ -0,0 +1,12 @@
+for number in range(1, 11):
+    if number % 2 == 0:
+        print(number, "- Чётное")
+    else:
+        print(number,"- Нечётное")
+
+
+count = 5
+while count >= 1:
+    print(count)
+    count -= 1
+print("Старт")
