521e843308932819565ecd38bbe9a5cd85424115
Adilbek <adilbekov0230@gmail.com>
2026-06-26 00:41:47 +0500

    Add day05: str method and f-str

---
+++ day05.py
@@ -0,0 +1,15 @@
+phrase = "  Python для тестировщиков   "
+print(phrase.strip())
+print(phrase.upper())
+print(len(phrase.strip()))
+print(phrase.strip().startswith("Python"))
+
+
+name = "dzostik" 
+age = 150
+city = "dno"
+print(f"Меня зовут {name}, мне {age}, Я из города {city}")
+
+price = 1500
+count = 3
+print(f"Колличество {count}, Цена {price}, Итого {count * price}")
